4fbf04940f87856f76a3469f555f1b7f8994278f
Kourosh-314 <138577426+Kourosh-314@users.noreply.github.com>
2025-09-15 16:47:19 +0330

    templates file
    I created the templates file.

--- myapp/views.py
+++ myapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def runserver(request):
-    return render(request,"app/home.html")
+    return render(request,"home.html")
