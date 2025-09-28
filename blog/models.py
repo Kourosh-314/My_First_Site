55b34eb5d6316b7bb618c45fa421947ad693f8cc
Kourosh-314 <138577426+Kourosh-314@users.noreply.github.com>
2025-09-28 15:40:28 +0330

    Revert "Adding models."
    This reverts commit a25767de1a4b2d0a9e4fbdfae25d7595476903dc.

--- blog/models.py
+++ blog/models.py
@@ -3,14 +3,5 @@ from django.db import models
 # Create your models here.
 
 class post(models.Model):
-    #author
-    #image
     title = models.CharField(max_length=250)
     content = models.TextField()
-    #category
-    #tag
-    counted_views = models.BooleanField(default=0)
-    status = models.BooleanField(default=False)
-    published_time = models.DateTimeField(null=True)
-    created_time = models.DateTimeField(auto_now_add=True)
-    updated_time = models.DateTimeField(auto_now=True)
