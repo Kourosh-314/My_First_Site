060b2e3b86320f9e2f35b20ec7c51bd869bf79cf
Kourosh-314 <138577426+Kourosh-314@users.noreply.github.com>
2025-09-28 15:42:11 +0330

    Ordering models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    #author
+    #image
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    #category
+    #tag
+    counted_views = models.BooleanField(default=0)
+    status = models.BooleanField(default=False)
+    published_time = models.DateTimeField(null=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
