5e9e651e6cf24566575ab03fc7330fbb49fe57f1
Kourosh-314 <138577426+Kourosh-314@users.noreply.github.com>
2025-09-28 15:40:20 +0330

    Revert "change the name from post to Post"
    This reverts commit cecbed3d65e6fcd652cb865c5389c8b827746e77.

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Post(models.Model):
+class post(models.Model):
     #author
     #image
     title = models.CharField(max_length=250)
