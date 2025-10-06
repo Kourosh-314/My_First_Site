eeb2e8425d006eb12974c6dc4e4590d98ad197b5
Kourosh-314 <138577426+Kourosh-314@users.noreply.github.com>
2025-10-06 16:49:27 +0330

    adding numberer.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from blog.models import Post
 from django.utils import timezone
+
 # Create your views here.
+#def for incresing the number of views
+def increase_views(posts):
+    for post in posts:
+        post.counted_views +=1
+        post.save(update_fields=['counted_views'])
+        
 def blog_home(request):
     now = timezone.now().date()
     posts = Post.objects.filter(published_time__lte=now)
+    increase_views(posts)
     #posts = Post.objects.all()
     context = {'posts':posts}
     return render(request,'Blog/blog-home.html',context)
@@ -18,4 +26,5 @@ def test(request):
     #posts = Post.objects.filter()
     context = {'posts':posts}
     return render(request,'test.html',context)
+
     
