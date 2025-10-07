3ce8289e92dd943ccc4d3f1d6b3c96f29a9c9699
Kourosh-314 <138577426+Kourosh-314@users.noreply.github.com>
2025-10-07 18:48:48 +0330

    adding time distributor correctly.

--- blog/views.py
+++ blog/views.py
@@ -3,16 +3,23 @@ from blog.models import Post
 from django.utils import timezone
 
 # Create your views here.
-#def for increasing the number of views        
+#def for increasing the number of views  
+def increase_views(posts):      
+    posts.counted_views +=1
+    posts.save(update_fields=['counted_views'])
+
 def blog_home(request):
-    posts = get_list_or_404(Post)
+    #posts = get_list_or_404(Post)
     #posts = Post.objects.all()
+    now = timezone.now()
+    posts = Post.objects.filter(published_time__lte = now , status = 1)
     context = {'posts':posts}
     return render(request,'Blog/blog-home.html',context)
 
 def blog_single(request,pid):
-    posts = get_list_or_404(Post,pk=pid)
+    #posts = get_list_or_404(Post,pk=pid)
     posts = Post.objects.get(pk=pid)
+    increase_views(posts)
     context = {'posts':posts}
     return render(request,'Blog/blog-single.html',context)
 
