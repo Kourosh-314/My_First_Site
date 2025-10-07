d92e6008b6468aa058e19fe04a4385bd746bfff6
Kourosh-314 <138577426+Kourosh-314@users.noreply.github.com>
2025-10-07 19:02:09 +0330

    .

--- blog/views.py
+++ blog/views.py
@@ -9,15 +9,12 @@ def increase_views(posts):
     posts.save(update_fields=['counted_views'])
 
 def blog_home(request):
-    #posts = get_list_or_404(Post)
-    #posts = Post.objects.all()
     now = timezone.now()
     posts = Post.objects.filter(published_time__lte = now , status = 1)
     context = {'posts':posts}
     return render(request,'Blog/blog-home.html',context)
 
 def blog_single(request,pid):
-    #posts = get_list_or_404(Post,pk=pid)
     posts = Post.objects.get(pk=pid)
     increase_views(posts)
     context = {'posts':posts}
