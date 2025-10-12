a9ebd2c4c0461f57a5441887f64131d5a0ecaaa2
Kourosh-314 <138577426+Kourosh-314@users.noreply.github.com>
2025-10-12 19:19:09 +0330

    Adding category and Image atributtes to model.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post , Category
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -8,4 +8,5 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title','author','counted_views','status','published_time','created_time')
     list_filter = ('status','author')
     search_fields = ['title','content']
-    
+
+admin.site.register(Category)
