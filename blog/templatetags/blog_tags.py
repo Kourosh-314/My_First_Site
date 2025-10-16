fa8770b995c0be4e96a4e4ec5bd5497183f39435
Kourosh-314 <138577426+Kourosh-314@users.noreply.github.com>
2025-10-16 20:03:54 +0330

    Adding filter template tag
    I added my first template tag.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -7,3 +7,7 @@ register = template.Library()
 def function():
     posts = Post.objects.filter(status = 1).count()
     return posts
+
+@register.filter
+def snippet(value,arg = 10):
+    return value[:arg]
