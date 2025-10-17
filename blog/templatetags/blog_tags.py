5e519fd45b984f6e188c8f50f19040ef6c136624
Kourosh-314 <138577426+Kourosh-314@users.noreply.github.com>
2025-10-17 17:47:07 +0330

    Adding latest posts widget.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -12,7 +12,7 @@ def function():
 def snippet(value,arg = 10):
     return value[:arg]
 
-@register.inclusion_tag('popularposts.html')
-def popularposts():
-    posts = Post.objects.filter(status=1).order_by('published_time')[:3]
+@register.inclusion_tag('Blog/blog-latest-posts.html')
+def latestposts(arg=3):
+    posts = Post.objects.filter(status=1).order_by('published_time')[:arg]
     return {'posts':posts}
