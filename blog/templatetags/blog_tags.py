1ae90a20ed939e6642261d2493b48c25a5289f88
Kourosh-314 <138577426+Kourosh-314@users.noreply.github.com>
2025-10-17 18:32:50 +0330

    Adding post categories field.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from blog.models import Post
-
+from blog.models import Category
 register = template.Library()
 
 @register.simple_tag(name = 'totalposts')
@@ -16,3 +16,12 @@ def snippet(value,arg = 10):
 def latestposts(arg=3):
     posts = Post.objects.filter(status=1).order_by('published_time')[:arg]
     return {'posts':posts}
+
+@register.inclusion_tag('Blog/blog-single-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories':cat_dict}
