ba7f385c7acc48a0a4153ce0502dd2da3da17777
Kourosh-314 <138577426+Kourosh-314@users.noreply.github.com>
2025-10-17 16:52:46 +0330

    Adding next and previous post emphasizer.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 from django.utils import timezone
-
+now = timezone.now()
 # Create your views here.
 #def for increasing the number of views.  
 def increase_views(post):      
@@ -9,15 +9,30 @@ def increase_views(post):
     post.save(update_fields=['counted_views'])
 
 def blog_home(request):
-    now = timezone.now()
     posts = Post.objects.filter(published_time__lte = now , status = 1)
     context = {'posts':posts}
     return render(request,'Blog/blog-home.html',context)
 
 def blog_single(request,pid):
     post = get_object_or_404(Post,pk = pid,status = 1)
+    posts = list(Post.objects.filter(published_time__lte=now, status=1))
+    next_post = None
+    previous_post = None
+    try:
+        current_post = next(post for post in posts if post.id == pid)
+        current_index = posts.index(current_post)
+        
+        if current_index < len(posts) - 1:
+            next_post = posts[current_index + 1]
+    
+        if current_index > 0:
+            previous_post = posts[current_index - 1]
+
+    except StopIteration:
+        
+        pass
     increase_views(post)
-    context = {'post':post}
+    context = {'post':post,'posts':posts,'next_post':next_post,'previous_post':previous_post}
     return render(request,'Blog/blog-single.html',context)
 
 def test(request):
