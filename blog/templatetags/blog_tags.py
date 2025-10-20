09e4499e57424b4a8aed001192976d0abdcc42c7
Kourosh-314 <138577426+Kourosh-314@users.noreply.github.com>
2025-10-20 20:59:15 +0330

    Adding posts in home page.
    I built and added the needed inclusion tag for home page post emphasizer.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -22,9 +22,14 @@ def latestposts(arg=3):
 
 @register.inclusion_tag('Blog/blog-single-post-categories.html')
 def postcategories():
-    posts = Post.objects.filter(status=1,published_time__lte = now)
+    posts = Post.objects.filter(status = 1,published_time__lte = now)
     categories = Category.objects.all()
     cat_dict = {}
     for name in categories:
         cat_dict[name] = posts.filter(category__name = name).count()
     return {'categories':cat_dict}
+
+@register.inclusion_tag("website/latest_post.html")
+def latest_posts(arg=6):
+    posts = Post.objects.filter(status = 1,published_time__lte = now).order_by('published_time')[:arg]
+    return {"posts":posts}
