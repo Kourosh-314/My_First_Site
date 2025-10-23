1f6946f1af007f3472bac0ce446f2f752cb69a1b
Kourosh-314 <138577426+Kourosh-314@users.noreply.github.com>
2025-10-23 16:26:03 +0330

    Adding debugtoolbar.

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django_extensions',
     'django.contrib.sites',
     'django.contrib.sitemaps',
-    'robots'
+    'robots',
+    'debug_toolbar',
 ]
 #sites framework
 SITE_ID = 2
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -139,3 +141,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
